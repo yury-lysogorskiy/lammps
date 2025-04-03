1892189eaa7fdaeb8242c6efd5dc7cbf7efba957
Axel Kohlmeyer <akohlmey@gmail.com>
2025-04-03 18:46:16 -0400

    looking for libcurl components fails for some installations

--- cmake/Modules/Packages/EXTRA-COMMAND.cmake
+++ cmake/Modules/Packages/EXTRA-COMMAND.cmake
@@ -1,9 +1,9 @@
 # the geturl command needs libcurl
 
-find_package(CURL QUIET COMPONENTS HTTP HTTPS)
+find_package(CURL QUIET)
 option(WITH_CURL "Enable libcurl support" ${CURL_FOUND})
 if(WITH_CURL)
-  find_package(CURL REQUIRED COMPONENTS HTTP HTTPS)
+  find_package(CURL REQUIRED)
   target_compile_definitions(lammps PRIVATE -DLAMMPS_CURL)
   target_link_libraries(lammps PRIVATE CURL::libcurl)
 endif()
