809d5b5b93a2a0413c5e61e272ad04b842fe430f
Evangelos Voyiatzis <evoyiatzis@gmail.com>
2025-04-07 21:08:35 +0200

    Fix typo in dihedral_charmm.cpp

--- src/MOLECULE/dihedral_charmm.cpp
+++ src/MOLECULE/dihedral_charmm.cpp
@@ -440,7 +440,7 @@ void DihedralCharmm::write_data(FILE *fp)
  void *DihedralCharmm::extract(const char *str, int &dim)
  {
    dim = 1;
-   if (strcmp(str, "k") == 0) return (void *) k0;
+   if (strcmp(str, "k") == 0) return (void *) k;
    if (strcmp(str, "n") == 0) return (void *) multiplicity;
    if (strcmp(str, "d") == 0) return (void *) shift;
    return nullptr;
