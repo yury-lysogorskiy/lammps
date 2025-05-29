3dadcab0476230e37e99636199ed8822a6f5b953
Axel Kohlmeyer <akohlmey@gmail.com>
2025-05-29 05:04:20 -0400

    remove dead code and redundant implementation.

--- src/MC/fix_neighbor_swap.h
+++ src/MC/fix_neighbor_swap.h
@@ -87,8 +87,7 @@ class FixNeighborSwap : public Fix {
   int attempt_swap();
   double energy_full();
   int pick_i_swap_atom();
-  int pick_j_swap_neighbor(int);
-  double get_distance(double[3], double[3]);
+  int pick_j_swap_neighbor();
   void build_i_neighbor_list(int);
   void update_iswap_atoms_list();
 };
