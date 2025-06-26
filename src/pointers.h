5ad24fc73342102015afe18cba5a40342494b582
Axel Kohlmeyer <akohlmey@gmail.com>
2025-06-26 00:22:59 -0400

    restore old version and add comment why it cannot be modernized

--- src/pointers.h
+++ src/pointers.h
@@ -92,7 +92,7 @@ class Pointers {
     atomKK(ptr->atomKK),
     memoryKK(ptr->memoryKK),
     python(ptr->python) {}
-  virtual ~Pointers() noexcept(false) = default;
+  virtual ~Pointers() noexcept(false) {};  // cannot use = default here due to broken GCC on RHEL 8
 
   // remove other default members where possible
 
